7d27177a007b8644d514e6f6bf6a76148043e67b
Milan White <milan.white.25@gmail.com>
2026-02-05 18:56:43 -0500

    feat: add basic navigation & incorporate tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { URLS } from "./config/navigation";
+
+import NotFoundPage from "../pages/General/NotFoundPage"
+
+const router = createBrowserRouter([
+    {
+        children: [
+            // Public Routes - for unauthenticated users
+
+            {
+                path: URLS.homePage,
+                element: (
+                    <>hello</>
+                ),
+            },
+
+            // 404
+            {
+                path: "*",
+                element: <NotFoundPage />,
+            },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
+);
