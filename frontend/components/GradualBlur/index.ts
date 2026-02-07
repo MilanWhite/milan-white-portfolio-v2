e217fd05fbe5469fc8734362f001b5fa22dd2a4e
Milan White <milan.white.25@gmail.com>
2026-02-07 11:06:40 -0500

    feat: add gradual blur

---
+++ frontend/components/GradualBlur/index.ts
@@ -0,0 +1,3 @@
+import GradualBlur from "./GradualBlur"
+
+export default GradualBlur;
--- frontend/pages/General/HomePage.tsx
+++ frontend/pages/General/HomePage.tsx
@@ -1,11 +1,22 @@
 import Landing from "../../components/Landing";
 import AboutMe from "../../components/AboutMe";
+import GradualBlur from "../../components/GradualBlur/GradualBlur";
 
 export default function HomePage() {
     return (
         <>
             <Landing />
             <AboutMe />
+            <GradualBlur
+                target="page"
+                position="bottom"
+                height="4rem"
+                strength={2}
+                divCount={5}
+                curve="bezier"
+                exponential
+                opacity={1}
+            />
         </>
     );
 }
